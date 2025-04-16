9b423b2318d37a49e5ee83b7c06d6b0b55ab4244
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-16 21:07:18 +0900

    operandB complete

--- src/level1/CalculatorLevel1.java
+++ src/level1/CalculatorLevel1.java
@@ -18,7 +18,8 @@ public class CalculatorLevel1 {
         for(int i = 0; i >= 0; i =+ 0) {        // 무한 반복문
 
         // 값 할당
-
+        System.out.println("------------------------------------------");
+        System.out.println("계산을 시작합니다.");
         System.out.println("계산할 숫자를 입력해주세요.");
         System.out.println("exit를 입력해 종료할 수 있습니다.");
 
@@ -44,14 +45,13 @@ public class CalculatorLevel1 {
                 System.out.println("0 이상의 정수를 입력해주세요.");
                 continue;
             }
-            decision
+            break;
         }
 
         System.out.println("원하는 연산 기호를 입력해주세요 (+ - * / 중 하나를 입력 해주세요)");
         System.out.println("exit를 입력해 종료할 수 있습니다.");
         System.out.print("연산 기호: ");
         operator = scanner.nextLine();       // operator 입력
-
 
         if (operator.equals("exit")) {      // operator exit 루프 탈출
             System.out.println("프로그램을 종료합니다");
@@ -60,21 +60,36 @@ public class CalculatorLevel1 {
 
         System.out.println("계산할 숫자를 입력해주세요.");
         System.out.println("exit를 입력해 종료할 수 있습니다.");
-        System.out.print("숫자: ");
-        operandB = scanner.nextLine();      // operandB 입력
 
+            while(decision) {
+                System.out.print("숫자: ");
+                operandB = scanner.nextLine();      // 유저가 직업 입력
 
-        if (operandB.equals("exit")) {      // operandB exit 루프 탈출
-            System.out.println("프로그램을 종료합니다");
-            break;
-        }
+                // for문 종료 조건 3
+                if (operandB.equals("exit")) {
+                    System.out.println("프로그램을 종료합니다");
+                    break;
+                }
+                // 정수 변환 2
+                try {
+                    operandD = Integer.parseInt(operandB);      // 문자열을 정수형으로 변환
+                } catch (NumberFormatException e) {     // 문자-> 정수 예외 처리
+                    System.out.println("숫자를 입력해주세요.");
+                    continue;
+                }
 
-        operandD = Integer.parseInt(operandB);
-        // 연산
-        // 제수가 0일 때 구분
-        if(operator.equals("/") && (operandD == 0)) {
-            System.out.println("제수에는 0이 들어갈 수 없습니다.\n다른 숫자를 입력해주세요");
-        } else {
+                // 음수 제외 2
+                if (operandD < 0) {
+                    System.out.println("0 이상의 정수를 입력해주세요.");
+                    continue;
+                }
+
+                if(operator.equals("/") && (operandD == 0)) {
+                    System.out.println("제수에는 0이 들어갈 수 없습니다.\n다른 숫자를 입력해주세요");
+                    continue;
+                }
+                break;
+            }
             switch (operator) {
                 case "+":
                     result = operandC + operandD;
@@ -96,5 +111,4 @@ public class CalculatorLevel1 {
         }
             }
         }
-    }
 
