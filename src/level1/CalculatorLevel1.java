a771bd0bd071acf7d5abb8b5a1227e407d629727
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-16 17:46:49 +0900

    ë³변수 설정 완료

--- src/level1/CalculatorLevel1.java
+++ src/level1/CalculatorLevel1.java
@@ -1,8 +1,17 @@
 package level1;
 
+import java.util.Scanner;
+
 public class CalculatorLevel1 {
 
     public static void main(String[] args) {
-        System.out.println("Hellow Calculator");
+
+        // 필수적인 변수 선언
+        Scanner scanner = new Scanner(System.in);
+        String operandA;
+        String operandB;
+        String opertor;
+
+
     }
 }
