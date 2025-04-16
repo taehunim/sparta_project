fc5d2a79f03434ba488c5eb788ff97c260ec64d8
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-16 20:45:00 +0900

    operandA complete

--- src/level1/CalculatorLevel1.java
+++ src/level1/CalculatorLevel1.java
@@ -11,22 +11,42 @@ public class CalculatorLevel1 {
         String operandA;
         String operandB;
         String operator;
+        int operandC = -1;
+        int operandD = -1;
         int result = -1; // 값이 할당되지 않는 경우를 막기위한 값 할당 , 초기화가 되지 않을 경우를 대비한 값 할당
-
+        boolean decision = true ;
         for(int i = 0; i >= 0; i =+ 0) {        // 무한 반복문
 
         // 값 할당
 
         System.out.println("계산할 숫자를 입력해주세요.");
         System.out.println("exit를 입력해 종료할 수 있습니다.");
-        System.out.print("숫자: ");
-        operandA = scanner.nextLine();      //operandA 입력
 
-        if (operandA.equals("exit")) {      // operandA exit 루프 탈출
-            System.out.println("프로그램을 종료합니다");
-            break;
+        while(decision) {
+            System.out.print("숫자: ");
+            operandA = scanner.nextLine();      // 유저가 직업 입력
+
+            // for문 종료 조건 1
+            if (operandA.equals("exit")) {
+                System.out.println("프로그램을 종료합니다");
+                break;
+            }
+            // 정수 변환 1
+            try {
+                operandC = Integer.parseInt(operandA);      // 문자열을 정수형으로 변환
+            } catch (NumberFormatException e) {     // 문자-> 정수 예외 처리
+                System.out.println("숫자를 입력해주세요.");
+                continue;
+            }
+
+            // 음수 제외 1
+            if (operandC < 0) {
+                System.out.println("0 이상의 정수를 입력해주세요.");
+                continue;
+            }
+            decision
         }
-        int operandC = Integer.parseInt(operandA);
+
         System.out.println("원하는 연산 기호를 입력해주세요 (+ - * / 중 하나를 입력 해주세요)");
         System.out.println("exit를 입력해 종료할 수 있습니다.");
         System.out.print("연산 기호: ");
@@ -49,7 +69,7 @@ public class CalculatorLevel1 {
             break;
         }
 
-        int operandD = Integer.parseInt(operandB);
+        operandD = Integer.parseInt(operandB);
         // 연산
         // 제수가 0일 때 구분
         if(operator.equals("/") && (operandD == 0)) {
