a829b63ad399160bcdd9b382b3e51d3cecd8f641
TaeHun Im <luxlaetitia9542@gmail.com>
2025-04-16 19:26:04 +0900

    add exit

--- src/level1/CalculatorLevel1.java
+++ src/level1/CalculatorLevel1.java
@@ -11,7 +11,7 @@ public class CalculatorLevel1 {
         String operandA;
         String operandB;
         String operator;
-        int result;
+        int result = -1; // 값이 할당되지 않는 경우를 막기위한 값 할당 , 초기화가 되지 않을 경우를 대비한 값 할당
 
         for(int i = 0; i >= 0; i =+ 0) {        // 무한 반복문
 
@@ -22,6 +22,10 @@ public class CalculatorLevel1 {
         System.out.print("숫자: ");
         operandA = scanner.nextLine();      //operandA 입력
 
+        if (operandA.equals("exit")) {      // operandA exit 루프 탈출
+            System.out.println("프로그램을 종료합니다");
+            break;
+        }
         int operandC = Integer.parseInt(operandA);
         System.out.println("원하는 연산 기호를 입력해주세요 (+ - * / 중 하나를 입력 해주세요)");
         System.out.println("exit를 입력해 종료할 수 있습니다.");
@@ -29,10 +33,21 @@ public class CalculatorLevel1 {
         operator = scanner.nextLine();       // operator 입력
 
 
+        if (operator.equals("exit")) {      // operator exit 루프 탈출
+            System.out.println("프로그램을 종료합니다");
+            break;
+        }
+
         System.out.println("계산할 숫자를 입력해주세요.");
         System.out.println("exit를 입력해 종료할 수 있습니다.");
         System.out.print("숫자: ");
         operandB = scanner.nextLine();      // operandB 입력
+
+
+        if (operandB.equals("exit")) {      // operandB exit 루프 탈출
+            System.out.println("프로그램을 종료합니다");
+            break;
+        }
 
         int operandD = Integer.parseInt(operandB);
         // 연산
